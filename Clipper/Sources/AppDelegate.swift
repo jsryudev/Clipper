5578142a860cb9c5c81710a5938c3518f20dd273
JunSang Ryu <jsryu.dev@gmail.com>
2020-10-27 23:12:33 +0900

    AppDelegate CompositionRoot 적용

--- Clipper/Sources/AppDelegate.swift
+++ Clipper/Sources/AppDelegate.swift
@@ -10,10 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+  var dependency: AppDependency!
+
   var window: UIWindow?
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    self.dependency = self.dependency ?? CompositionRoot.resolve()
+    self.dependency.configureSDKs()
+    self.window = self.dependency.window
     return true
   }
 }
