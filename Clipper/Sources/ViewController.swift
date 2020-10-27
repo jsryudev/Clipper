69a209f17e230a3950511ac52617f39c2cf15836
JunSang Ryu <jsryu.dev@gmail.com>
2020-10-27 21:56:46 +0900

    Project init

---
+++ Clipper/Sources/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Clipper
+//
+//  Created by JunSang Ryu on 2020/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
