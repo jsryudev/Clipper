e1c70c4062b1541ad9dd9de90d2d262d13cbb28a
JunSang Ryu <jsryu.dev@gmail.com>
2020-10-30 04:06:09 +0900

    AccessToken을 관리할 AuthService 추가

---
+++ Clipper/Sources/Services/AuthService.swift
@@ -0,0 +1,57 @@
+//
+//  AuthService.swift
+//  Clipper
+//
+//  Created by JunSang Ryu on 2020/10/30.
+//
+
+import Foundation
+
+import KeychainAccess
+
+protocol AuthServiceType {
+  var accessToken: String? { get set }
+  func signOut()
+  func save(token: String?)
+}
+
+// FIXME
+final class AuthService: AuthServiceType {
+  private var _accessToken: String?
+  var accessToken: String? {
+    get {
+      return _accessToken ?? load()
+    }
+    set {
+      self._accessToken = newValue
+      self.save(token: newValue)
+    }
+  }
+
+  fileprivate let keychain = Keychain(service: "dev.jsryu.clipper")
+
+  init() {
+    self._accessToken = self.load()
+  }
+
+  func signOut() {
+    self.accessToken = nil
+    delete()
+  }
+
+  func save(token: String?) {
+    guard let accessToken = token else { return }
+    try? self.keychain.set(accessToken, key: "access_token")
+    self.accessToken = nil
+  }
+
+  fileprivate func load() -> String? {
+    guard let accessToken = self.keychain["access_token"] else { return nil }
+    return accessToken
+  }
+
+  fileprivate func delete() {
+    try? self.keychain.remove("access_token")
+  }
+}
+
