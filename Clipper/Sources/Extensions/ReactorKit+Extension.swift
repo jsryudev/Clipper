cf75050fa360e6063ba85357dcfc163ff0ef8f0f
JunSang Ryu <jsryu.dev@gmail.com>
2020-11-02 05:55:17 +0900

    ReactorKit+Extension 추가

---
+++ Clipper/Sources/Extensions/ReactorKit+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  ReactorKit+Extension.swift
+//  Clipper
+//
+//  Created by JunSang Ryu on 2020/11/02.
+//
+
+import RxCocoa
+import RxSwift
+import ReactorKit
+
+struct TrackedValue<T> {
+  var tracker: Int
+  var value: T
+}
+
+extension ObservableType {
+  func mapChangedTrackedValue<T> (_ transform: @escaping (Element) throws -> TrackedValue<T>) -> Observable<T> {
+        return self
+            .map(transform)
+            .distinctUntilChanged { $0.tracker == $1.tracker }
+            .map { $0.value }
+    }
+}
