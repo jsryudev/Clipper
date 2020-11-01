3ab9f83e2e728a7d15e4e7716ea068c47d615da0
JunSang Ryu <jsryu.dev@gmail.com>
2020-11-02 04:14:19 +0900

    MainViewController 추가

---
+++ Clipper/Sources/ViewControllers/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  Clipper
+//
+//  Created by JunSang Ryu on 2020/11/02.
+//
+
+import UIKit
+
+import SnapKit
+import ReactorKit
+import RxSwift
+import RxCocoa
+import RxViewController
+
+class MainViewController: BaseViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func addSubViews() {
+  }
+
+  override func setupConstraints() {
+
+  }
+
+  // MARK: Initialize
+
+
+  required convenience init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
