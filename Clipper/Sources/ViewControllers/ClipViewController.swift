1edf67e90b9fc594f9ea80bee8684b9cfc9b0265
JunSang Ryu <jsryu.dev@gmail.com>
2020-11-11 16:25:26 +0900

    ClipViewController Header 구현

--- Clipper/Sources/ViewControllers/ClipViewController.swift
+++ Clipper/Sources/ViewControllers/ClipViewController.swift
@@ -8,17 +8,60 @@
 import UIKit
 
 import ReactorKit
+import ReusableKit
+import RxDataSources
 
 class ClipViewController: BaseViewController, View {
   typealias Reactor = ClipViewReactor
+  
 
-  fileprivate let tableView: UITableView = {
-    let view = UITableView()
+  fileprivate struct Reusable {
+    static let actionCell = ReusableCell<ClipViewActionCell>()
+  }
+
+  fileprivate let dataSource: RxTableViewSectionedReloadDataSource<ClipViewSection>
+
+  let tableView: UITableView = {
+    let view = UITableView(frame: .zero, style: .grouped)
+    view.backgroundColor = .clear
+    view.register(Reusable.actionCell)
     return view
   }()
 
+  // MARK: Initialize
+
+  init(reactor: Reactor) {
+    defer { self.reactor = reactor }
+    self.dataSource = type(of: self).dataSourceFactory()
+    super.init()
+  }
+
+  required convenience init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private static func dataSourceFactory() -> RxTableViewSectionedReloadDataSource<ClipViewSection> {
+    return .init(
+      configureCell: { dataSource, tableView, indexPath, sectionItem in
+        let cell: UITableViewCell
+        switch sectionItem {
+        case .action:
+          let actionCell = tableView.dequeue(Reusable.actionCell, for: indexPath)
+          cell = actionCell
+        default:
+          cell = UITableViewCell()
+        }
+        return cell
+      },
+      titleForHeaderInSection: { dataSource, index  in
+        return dataSource.sectionModels[index].title
+      }
+    )
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.view.backgroundColor = .clear
   }
 
   override func addSubViews() {
@@ -31,17 +74,9 @@ class ClipViewController: BaseViewController, View {
     }
   }
 
-  // MARK: Initialize
-
-  init(reactor: Reactor) {
-    defer { self.reactor = reactor }
-    super.init()
-  }
-
-  required convenience init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
   func bind(reactor: ClipViewReactor) {
+    reactor.state.map { $0.sections }
+      .bind(to: self.tableView.rx.items(dataSource: self.dataSource))
+      .disposed(by: self.disposeBag)
   }
 }
