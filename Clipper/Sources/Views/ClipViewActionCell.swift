bf2233db2070c4a537e11b6584edd0ff19261a96
JunSang Ryu <jsryu.dev@gmail.com>
2020-11-11 16:23:05 +0900

    ClipViewActionCell 추가

---
+++ Clipper/Sources/Views/ClipViewActionCell.swift
@@ -0,0 +1,35 @@
+//
+//  ClipViewActionCell.swift
+//  Clipper
+//
+//  Created by JunSang Ryu on 2020/11/11.
+//
+
+import UIKit
+
+import SnapKit
+import ReactorKit
+
+final class ClipViewActionCell: BaseTableViewCell{
+
+  fileprivate let actionButton: UIButton = {
+    let button = UIButton()
+    button.backgroundColor = .clear
+    button.setTitle("추가", for: .normal)
+    return button
+  }()
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    self.contentView.addSubview(actionButton)
+    self.backgroundColor = .clear
+  }
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+
+    self.actionButton.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+    }
+  }
+}
