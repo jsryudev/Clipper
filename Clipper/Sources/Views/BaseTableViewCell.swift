781aaa4f7363333a6230819d46caf410f57f2563
JunSang Ryu <jsryu.dev@gmail.com>
2020-11-11 16:21:39 +0900

    BaseTableViewCell 추가

---
+++ Clipper/Sources/Views/BaseTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  BaseTableViewCell.swift
+//  Clipper
+//
+//  Created by JunSang Ryu on 2020/11/11.
+//
+
+import UIKit
+
+import RxSwift
+
+class BaseTableViewCell: UITableViewCell {
+
+  var disposeBag = DisposeBag()
+
+  // MARK: Initializing
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+
+  required convenience init?(coder aDecoder: NSCoder) {
+    self.init(style: .default, reuseIdentifier: nil)
+  }
+}
