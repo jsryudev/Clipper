7fcefed1299293b6d352b2797f10809a762ac1ea
JunSang Ryu <jsryu.dev@gmail.com>
2020-11-28 15:47:13 +0900

    ClipListViewReactor Section 구현

--- Clipper/Sources/ViewControllers/ClipListViewReactor.swift
+++ Clipper/Sources/ViewControllers/ClipListViewReactor.swift
@@ -21,16 +21,50 @@ final class ClipListViewReactor: Reactor {
 
   struct State {
     let markerID: String
+    var sections: [ClipListViewSection]
   }
 
   let initialState: State
   let clipService: ClipServiceType
+  let clipCellReactorFactory: (Clip) -> ClipCellReactor
 
   init(
     marker id: String,
-    clipService: ClipServiceType
+    clipService: ClipServiceType,
+    clipCellReactorFactory: @escaping (Clip) -> ClipCellReactor
   ) {
     self.clipService = clipService
-    self.initialState = State(markerID: id)
+    self.clipCellReactorFactory = clipCellReactorFactory
+    self.initialState = State(markerID: id, sections: [])
+  }
+
+  func mutate(action: Action) -> Observable<Mutation> {
+    switch action {
+    case .refresh:
+      return self.clipService
+        .fetchClips(
+          marker: currentState.markerID,
+          page: 0,
+          limit: 0
+        )
+        .asObservable()
+        .map { .setClips($0) }
+    }
+  }
+
+  func reduce(state: State, mutation: Mutation) -> State {
+    var newState = state
+    switch mutation {
+    case .setClips(let clips):
+      let items =  self.clipSectionItems(with: clips)
+      newState.sections = [.clip(items)]
+    }
+    return newState
+  }
+
+  private func clipSectionItems(with clips: [Clip]) -> [ClipListViewSectionItem] {
+    return clips
+      .map(self.clipCellReactorFactory)
+      .map(ClipListViewSectionItem.clip)
   }
 }
